2edf4eddcb7c686adba63fe54286b3e24e63f03b
khalid alade <96583134+khalid-alade@users.noreply.github.com>
2022-08-30 11:06:36 +0300

    Update cred.py

--- cred.py
+++ cred.py
@@ -1,4 +1,4 @@
-SPOTIPY_CLIENT_ID= 'a7d9f6c4c80943f0900db19fbb88ce9d'
-SPOTIPY_CLIENT_SECRET='5a8ef29c5e664ed1be6ecd7f7f220495'
+SPOTIPY_CLIENT_ID= {USe your credentials}
+SPOTIPY_CLIENT_SECRET={USe your credentials}
 SPOTIPY_REDIRECT_URI='http://127.0.0.1:5000'
 SCOPE = 'user-follow-read ugc-image-upload user-read-playback-state user-modify-playback-state user-read-currently-playing user-read-private user-read-email user-follow-modify user-follow-read user-library-modify user-library-read streaming app-remote-control user-read-playback-position user-top-read user-read-recently-played playlist-modify-private playlist-read-collaborative playlist-read-private playlist-modify-public'
