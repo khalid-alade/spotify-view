b0d74e881e5a42adef572ba3c45b38385ce35ec9
Yusuf Khalid <khalidalade2004@gmail.com>
2022-09-10 18:18:13 +0300

    fixed the redirect URIs

--- spotify_app/routes.py
+++ spotify_app/routes.py
@@ -24,7 +24,7 @@ def get_artist(access_token, id):
 @app.route('/login', methods=['GET'])
 def login():
     authentication_request_params = {
-    'client_id': SPOTIPY_CLIENT_ID,'response_type': 'code','redirect_uri': 'http://127.0.0.1:5000/profile',
+    'client_id': SPOTIPY_CLIENT_ID,'response_type': 'code','redirect_uri': 'https://spotify-view.herokuapp.com/profile',
     'state': 'RAndomstrinGRandOmstring',"show_dialog": 'true','scope': SCOPE}
     auth_url = 'https://accounts.spotify.com/en/authorize?' + urlencode(authentication_request_params)
 
@@ -34,7 +34,7 @@ def login():
 @app.route('/profile')
 def index():
     auth_token = request.args['code']
-    code_payload = {"grant_type": "authorization_code","code": str(auth_token),"redirect_uri": 'http://127.0.0.1:5000/profile',
+    code_payload = {"grant_type": "authorization_code","code": str(auth_token),"redirect_uri": 'https://spotify-view.herokuapp.com/profile',
     'client_id': SPOTIPY_CLIENT_ID,'client_secret': SPOTIPY_CLIENT_SECRET,}
     
     post_request = requests.post('https://accounts.spotify.com/api/token', data=code_payload)
